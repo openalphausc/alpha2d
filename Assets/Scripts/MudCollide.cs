b6a1c1593b7ed0394476328ecfe617d113ba781c
Colin Spiridonov <spiridon@usc.edu>
2020-02-18 22:46:52 -0800

    Fixed bugs so that wiper appropriately cleans mud splotches

--- Assets/Scripts/MudCollide.cs
+++ Assets/Scripts/MudCollide.cs
@@ -6,8 +6,10 @@ public class MudCollide : MonoBehaviour
 {
     void OnTriggerStay2D(Collider2D other)
     {
-        if (other.name == "Wiper" && Input.GetKey("Space"))
+      Debug.Log("Colliding");
+        if (other.name == "Wiper" && Input.GetKey("space"))
         {
+          Debug.Log("Colliding with wiper and space.");
             Destroy(gameObject);
         }
     }
