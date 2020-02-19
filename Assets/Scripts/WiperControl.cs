62d7b27ddcbaa730e476544d5ed1fd4c8c8a9cb9
Colin Spiridonov <spiridon@usc.edu>
2020-02-18 20:25:24 -0800

    Added WiperControl script that moves the wiper blade using arrow keys

--- Assets/Scripts/WiperControl.cs
+++ Assets/Scripts/WiperControl.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class WiperControl : MonoBehaviour
 {
+    public float speed;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 0.1f;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      float moveX = 0;
+      float moveY = 0;
+      if(Input.GetKey(KeyCode.RightArrow)) moveX = speed;
+      else if(Input.GetKey(KeyCode.LeftArrow)) moveX = -speed;
+      if(Input.GetKey(KeyCode.UpArrow)) moveY = speed;
+      else if(Input.GetKey(KeyCode.DownArrow)) moveY = -speed;
+      transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY);
     }
 }
