c18005b1e2b4d3c997cc8a5976e200d53e4f697b
Colin Spiridonov <spiridon@usc.edu>
2020-02-18 22:41:40 -0800

    Made wiper move at an appropriate speed and move with the moving body

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,60 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public Animator animator;
-
     private Rigidbody2D rb;
-    private Vector2 moveVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        speed = 0.07f;
     }
 
     void OnCollisionEnter2D(Collision2D dataFromCollision)
     {
-    
+
         string name = dataFromCollision.gameObject.name;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        
-            Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-            moveVelocity = moveInput.normalized * speed;
-            if (Input.GetAxis("Vertical") > 0)
-            {
-                animator.SetFloat("VerticalSpeed", 1);
-            }
-            else if (Input.GetAxis("Vertical") < 0)
-            {
-                animator.SetFloat("VerticalSpeed", -1);
-            }
-            if (Input.GetAxis("Horizontal") > 0)
-            {
-                if (Input.GetAxis("Vertical") == 0)
-                {
-                    animator.SetFloat("HorizontalSpeed", 1);
-                }
-            }
-            else if (Input.GetAxis("Horizontal") < 0)
-            {
-                if (Input.GetAxis("Vertical") == 0)
-                {
-                    animator.SetFloat("HorizontalSpeed", -1);
-                }
-            }
-        
-    }
-
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
-        animator.SetFloat("VerticalSpeed", 0);
-        animator.SetFloat("HorizontalSpeed", 0);
+      float moveX = 0;
+      float moveY = 0;
+      if(Input.GetKey("d")) moveX = speed;
+      else if(Input.GetKey("a")) moveX = -speed;
+      transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY);
     }
 }
--- Assets/Scripts/WiperControl.cs
+++ Assets/Scripts/WiperControl.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class WiperControl : MonoBehaviour
 {
-    public float speed;
+    public float wiperSpeed;
     private Rigidbody2D rb;
+    private PlayerController playerScript;
+    private float playerSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 0.1f;
+        wiperSpeed = 0.1f;
         rb = GetComponent<Rigidbody2D>();
+        playerScript = GameObject.Find("Body").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -19,10 +22,15 @@ public class WiperControl : MonoBehaviour
     {
       float moveX = 0;
       float moveY = 0;
-      if(Input.GetKey(KeyCode.RightArrow)) moveX = speed;
-      else if(Input.GetKey(KeyCode.LeftArrow)) moveX = -speed;
-      if(Input.GetKey(KeyCode.UpArrow)) moveY = speed;
-      else if(Input.GetKey(KeyCode.DownArrow)) moveY = -speed;
+      playerSpeed = playerScript.speed;
+      // If player is moving, move at the same speed as the player
+      if(Input.GetKey("d")) moveX += playerSpeed; // make "speed" the player object's speed
+      else if(Input.GetKey("a")) moveX -= playerSpeed;
+      // If input is given, move the wiper
+      if(Input.GetKey(KeyCode.RightArrow)) moveX += wiperSpeed;
+      else if(Input.GetKey(KeyCode.LeftArrow)) moveX -= wiperSpeed;
+      if(Input.GetKey(KeyCode.UpArrow)) moveY += wiperSpeed;
+      else if(Input.GetKey(KeyCode.DownArrow)) moveY -= wiperSpeed;
       transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY);
     }
 }
